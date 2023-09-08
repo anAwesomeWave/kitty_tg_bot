8860c1038b059668ddd557f6044e780500fbc950
Tim Zubov <timofey.zubov.05@gmail.com>
2023-09-08 18:26:11 +0300

    Add info handler

--- core/bot.py
+++ core/bot.py
@@ -2,9 +2,10 @@ import os
 import logging
 
 from telegram.ext import Filters, CommandHandler, Updater, MessageHandler
+from telegram import ReplyKeyboardMarkup
 from dotenv import load_dotenv
 
-from utils import get_random_cat_photo
+from utils import get_random_cat_photo, get_info_text
 
 # по угару добавить бд со ввсеми юзерами, которые пользовались когда-либо ботом
 # добавлять id стикеров в бд
@@ -15,18 +16,26 @@ load_dotenv()
 
 def start_bot(update, context):
     chat_id = update.effective_chat.id
+    buttons = ReplyKeyboardMarkup([
+        ['/info', '/get_cat_photo', '/start']
+    ], resize_keyboard=True)
     context.bot.send_message(
         # INFO новый чат с юзером {id} и {username}
         chat_id=chat_id,
         text='Спасибо, что выбрали самого эффективного в мире '
              'картинко-кошко-отслылочного бота)'
-             '\nмяу!😽'
+             '\nмяу!😽',
+        reply_markup=buttons
     )
     context.bot.send_sticker(
         chat_id=chat_id,
         sticker='CAACAgUAAxkBAANbZPsscN_uL03H_2v'
                 'nJOQdmQy1ohYAAu4BAAKSHShXrArQVp8VXLIwBA'
     )
+
+
+def send_info(update, context):
+    context.bot.send_message(update.effective_chat.id, get_info_text())
 
 
 def send_cat_photo(update, context):
@@ -43,10 +52,10 @@ def get_id_of_sticker(update, context):
 
 updater = Updater(token=os.getenv('BOT_API_KEY'))
 
-
 updater.dispatcher.add_handler(CommandHandler('start', start_bot))
 updater.dispatcher.add_handler(CommandHandler('get_cat_photo', send_cat_photo))
-
+updater.dispatcher.add_handler(CommandHandler('info', send_info))
+updater.dispatcher.add_handler(MessageHandler(Filters.all, send_info))
 
 if __name__ == '__main__':
     updater.dispatcher.add_handler(
